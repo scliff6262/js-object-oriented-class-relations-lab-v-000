083e155bbe3eadc8f0d9575de19086f5c571192c
Sean Clifford <sclifford6262@gmail.com>
2018-05-08 23:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ class Trip {
   constructor(driver, passenger){
     this.id = ++tripId
     this.driverId = driver["id"]
-    this.passengerId = passenger.id
+    this.passengerId = passenger["id"]
     store.trips.push(this)
   }
 
